c748bbd2c940dea0904a3a023ae5ea53f0e7ca24
minzy <minzy626@gmail.com>
2017-06-12 10:24:52 +0900

    150612

--- app/src/main/java/com/example/minji/ticketing/CommentActivity.java
+++ app/src/main/java/com/example/minji/ticketing/CommentActivity.java
@@ -1,6 +1,7 @@
 package com.example.minji.ticketing;
 
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -13,6 +14,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -21,6 +23,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -40,6 +43,7 @@ public class CommentActivity extends AppCompatActivity {
     EditText et_postcomment;
     ImageButton btn_input;
     String comment;
+    static String deal_index;
 
     private List<HashMap<String,String>> comment_List=null;
     ListView listView=null;
@@ -50,8 +54,8 @@ public class CommentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
         final Intent intent = getIntent();
-        String deal_idx=intent.getExtras().getString("deal_idx");//전페이지에서 넘겨받은것 저장
-
+        final String deal_idx=intent.getExtras().getString("deal_idx");//전페이지에서 넘겨받은것 저장
+        deal_index=deal_idx;
         imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imageView2=(ImageView)findViewById(R.id.imageView2);
         btn_input=(ImageButton)findViewById(R.id.btn_input);
@@ -69,6 +73,8 @@ public class CommentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 comment=et_postcomment.getText().toString();
+                InsertData task = new InsertData();
+                task.execute( comment,LoginActivity.Index,deal_idx);
 
             }
         });
@@ -195,4 +201,106 @@ public class CommentActivity extends AppCompatActivity {
 
         return page.toString();
     }// getStringFromUrl()------------------------
+
+    class InsertData extends AsyncTask<String, Void, String> { //<onPre parameter, doInBack parameter, onPost parameter>
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() { //Asynctask 실행 했을 때 뜨는 화면
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(CommentActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {//Asynctask 완료 후 뜨는 화면
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            if (result.equals("SQL문 처리 성공")) {
+
+                et_postcomment.setText(""); //초기화
+
+                Toast.makeText(CommentActivity.this, "등록 완료 되었습니다", Toast.LENGTH_SHORT).show();
+                Intent intent1 = new Intent(getApplicationContext(), CommentActivity.class);
+                intent1.putExtra("deal_idx",deal_index);
+                startActivity(intent1);
+                finish();
+            } else {
+                Toast.makeText(CommentActivity.this, "다시 시도해주세요", Toast.LENGTH_SHORT).show();
+            }
+            Log.d("Tag", "POST response  - " + result);
+        }
+
+        @Override
+        protected String doInBackground(String... params) { //background
+
+            String comment_data = (String) params[0]; // 여기에 댓글내용을 받아올거다.
+            String mem_idx = (String) params[1];
+            String deal_idx = (String) params[2];
+
+
+            String serverURL = "https://ticketing-php-minzykim.c9users.io/reply.php";
+            String postParameters = "com_contents=" + comment_data + "&mem_idx=" + mem_idx +"&deal_idx="+deal_idx;
+            Log.d("Tag", "POST response  - " + postParameters);
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                //httpURLConnection.setRequestProperty("content-type", "application/json");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d("Tag", "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d("TAG", "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
 }
